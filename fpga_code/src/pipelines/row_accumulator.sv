adbcc150306330da9b9ae4a82b04f72de886416d
Yang Yue <yyue1@ece001.ece.local.cmu.edu>
2020-05-05 13:30:27 -0400

    Fixed up multiplier

--- fpga_code/src/pipelines/row_accumulator.sv
+++ fpga_code/src/pipelines/row_accumulator.sv
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module multiplier 
+module multiplier1
 	#(parameter NUM_CHANNELS=4,
 		parameter MATRIX_SIZE=128)
 	(input logic clk, rst_l,
@@ -14,6 +14,8 @@ module multiplier
     output logic done);
 
 
+	/* Memory uses a full cycle of delay
+	 * Need to buffer values for a full cycle */
 	logic [NUM_CHANNELS-1:0][31:0] vector_values;
 	ROM mem0(.address_a(col_id[0][9:0]),
 			.address_b(col_id[1][9:0]),
@@ -27,60 +29,92 @@ module multiplier
 			.q_a(vector_values[2]),
 			.q_b(vector_values[3]));
 
+	/* Buffered values through the pipeline */
 	genvar k;
-	logic [NUM_CHANNELS-1:0][31:0] row_id_stored, matrix_values_stored;
+	logic [NUM_CHANNELS-1:0][31:0] row_id_stored0, matrix_values_stored0;
+	logic [NUM_CHANNELS-1:0][31:0] row_id_stored1, matrix_values_stored1;
+	logic rdy_stored0, rdy_stored1;
+
 	generate
-		for (k=0; k<NUM_CHANNELS;k++) begin
-			register row_id_store(
+		for (k=0; k<NUM_CHANNELS;k++) begin: store
+			register row_id_store0(
 				.clk, .rst_l,
 				.en(rdy),
+				.clear(1'b0),
 				.D(row_id[k]),
-				.Q(row_id_stored[k]));
-			register matrix_value_store(
+				.Q(row_id_stored0[k]));
+			register matrix_value_store0(
 				.clk,
 				.rst_l,
 				.en(rdy),
+				.clear(1'b0),
 				.D(values[k]),
-				.Q(matrix_values_stored[k]));
+				.Q(matrix_values_stored0[k]));
+			register row_id_store1(
+				.clk, .rst_l,
+				.en(rdy_stored0),
+				.clear(1'b0),
+				.D(row_id_stored0[k]),
+				.Q(row_id_stored1[k]));
+			register matrix_value_store1(
+				.clk,
+				.rst_l,
+				.en(rdy_stored0),
+				.clear(1'b0),
+				.D(matrix_values_stored0[k]),
+				.Q(matrix_values_stored1[k]));
 		end
 	endgenerate
+	register ready_store0(
+				.clk, 
+				.rst_l,
+				.en(rdy), 
+				.clear(1'b0),
+				.D(rdy),
+				.Q(rdy_stored0));
+	register ready_store1(
+				.clk, 
+				.rst_l,
+				.en(rdy_stored0), 
+				.clear(1'b0),
+				.D(rdy_stored0),
+				.Q(rdy_stored1));
 
+
+	/* Logic to calculate finished channels and multiplication */
 	logic [NUM_CHANNELS-1:0] channel_finished;
 	logic [NUM_CHANNELS-1:0][31:0] mult_values;
 	generate
-		for(k=0; k<NUM_CHANNELS; k++) begin
-			assign channel_finished[k] = (row_id_stored[k] >= MATRIX_SIZE);
+		for(k=0; k<NUM_CHANNELS; k++) begin: mul
+			assign channel_finished[k] = (row_id_stored1[k] >= MATRIX_SIZE);
 
 			MULT mul(
-				.dataa(matrix_values_stored[k]), // Matrix column value
+				.dataa(matrix_values_stored1[k]), // Matrix column value
 				.datab(vector_values[k]), // Vector column value
 				.result(mult_values[k]));
 		end
 	endgenerate
 
+	//assign done = channel_finished[0] & channel_finished[1] & channel_finished[2] & channel_finished[3];
 
-	typedef enum {IDLE, MULT, DONE} state_t;
+	typedef enum {MULT, DONE} state_t;
 	state_t state;
 
 	always_ff @(posedge clk, negedge rst_l) begin : proc_
 		if(~rst_l) begin
 			accum <= 0;
-			state <= IDLE;
+			state <= MULT;
 		end else begin
 			accum <= accum;
 			done <= 1'b0;
 			case (state) 
-				IDLE: begin
-					if (rdy) state <= MULT;
-					else state <= IDLE;
-				end
 				MULT: begin
 					for (int i = 0; i < NUM_CHANNELS; i++) begin
-						if (!channel_finished[i]) accum[row_id_stored[i]] <= accum[row_id_stored[i]] + mult_values[i];
+						if (!channel_finished[i] & rdy_stored1) accum[row_id_stored1[i]] <= accum[row_id_stored1[i]] + mult_values[i];
 					end
 
 					if (channel_finished[0] & channel_finished[1] & channel_finished[2] & channel_finished[3]) state <= DONE;
-					else state <= IDLE;
+					else state <= MULT;
 				end
 				DONE: begin 
 					done <= 1'b1;
@@ -89,4 +123,4 @@ module multiplier
 			endcase
 		end
 	end
-endmodule : multiplier
+endmodule : multiplier1
